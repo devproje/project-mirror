fdae02e77b25e5e1ffd18c10a64a75b6fb706cb9
Project_IO <me@projecttl.net>
2023-11-10 23:55:22 +0900

    feat: middle save

--- src/router/worker.go
+++ src/router/worker.go
@@ -3,45 +3,69 @@ package router
 import (
 	"fmt"
 	"html/template"
+	"math"
 	"os"
 	"strings"
 
-	"github.com/devproje/plog/log"
 	"github.com/devproje/project-mirror/src/config"
 	"github.com/gin-gonic/gin"
 )
 
-func createFileElement(path string, fileinfo os.FileInfo) string {
-	format := "2006-01-02 03:04 PM"
-	var filename string
-	modified := fileinfo.ModTime().Format(format)
-	if fileinfo.IsDir() {
-		filename = fileinfo.Name() + "/"
-	} else {
-		filename = fileinfo.Name()
+type FileData struct {
+	URL      string
+	Name     string
+	Size     string
+	Modified string
+}
+
+func arrToStr(arr []FileData) *string {
+	var str = ""
+	for _, i := range arr {
+		str += createElement(i.URL, i.Name, i.Size, i.Modified)
 	}
 
+	return &str
+}
+
+func getFileSize(size float64) string {
+	var suffixes = [5]string{"Bytes", "KB", "MB", "GB", "TB"}
+	if size == 0 {
+		return fmt.Sprintf("%.0f %s", size, suffixes[0])
+	}
+
+	base := math.Log(float64(size)) / math.Log(1024)
+	converted := math.Pow(1024, base-math.Floor(base))
+	suffix := suffixes[int(math.Floor(base))]
+
+	if size > 1023 {
+		return fmt.Sprintf("%.1f %s", converted, suffix)
+	}
+
+	return fmt.Sprintf("%.0f %s", converted, suffix)
+}
+
+func createElement(path, name, size, modified string) string {
 	return fmt.Sprintf(
-		`<a class="file" href="%s">
-			<p class="file_name">%s</p>
-			<p class="file_size">%d bytes</p>
-			<p class="file_modified">%s</p>
-		</a>`,
+		`<tr class="file">
+			<td><a href="%s">%s</a></td>
+			<td>%s</td>
+			<td>%s</td>
+		</tr>`,
 		path,
-		filename,
-		fileinfo.Size(),
+		name,
+		size,
 		modified,
 	)
 }
 
 func MirrorWorker(ctx *gin.Context, path string) {
+	sort, asc := ctx.GetQuery("sort")
 	if config.Get().Auth {
 		_, status := CheckLogin(ctx)
 		if status != 200 {
 			ctx.Redirect(301, "/v1/login")
 			return
 		}
-		log.Debugln("test")
 	}
 
 	iPath := fmt.Sprintf(".data/%s", path)
@@ -53,15 +77,13 @@ func MirrorWorker(ctx *gin.Context, path string) {
 		})
 		return
 	}
-	log.Debugln("test")
 
 	if !file.IsDir() {
 		ctx.FileAttachment(iPath, file.Name())
 		return
 	}
-	log.Debugln("test")
 
-	dir := ReadDir(path)
+	dir := read(path, sort, asc)
 	ctx.HTML(200, "index.html", gin.H{
 		"name":     config.Get().Name,
 		"dir_name": path,
@@ -69,9 +91,10 @@ func MirrorWorker(ctx *gin.Context, path string) {
 	})
 }
 
-func ReadDir(path string) *string {
+func read(path string, srt string, asc bool) *string {
 	dir, _ := os.ReadDir(fmt.Sprintf(".data/%s", path))
-	var back, files string
+	var back string
+	var files []FileData
 
 	if path != "/" {
 		split := strings.Split(path, "/")
@@ -91,14 +114,12 @@ func ReadDir(path string) *string {
 			back = "../"
 		}
 
-		files += fmt.Sprintf(
-			`<a class="file" href="%s">
-				<p class="file_name">../</p>
-				<p class="file_size"></p>
-				<p class="file_modified"></p>
-			</a>`,
-			back,
-		)
+		files = append(files, FileData{
+			URL:      back,
+			Name:     "../",
+			Size:     "-",
+			Modified: "-",
+		})
 	}
 
 	for _, file := range dir {
@@ -107,9 +128,26 @@ func ReadDir(path string) *string {
 			ph = fmt.Sprint(file.Name())
 		}
 
-		fileinfo, _ := file.Info()
-		files += createFileElement(ph, fileinfo)
+		format := "2006-01-02 03:04 PM"
+		var name, size string
+		finfo, _ := file.Info()
+		modified := finfo.ModTime().Format(format)
+		if file.IsDir() {
+			name = file.Name() + "/"
+			size = "-"
+			modified = "-"
+		} else {
+			name = file.Name()
+			size = getFileSize(float64(finfo.Size()))
+		}
+
+		files = append(files, FileData{
+			URL:      ph,
+			Name:     name,
+			Size:     size,
+			Modified: modified,
+		})
 	}
 
-	return &files
+	return arrToStr(files)
 }
